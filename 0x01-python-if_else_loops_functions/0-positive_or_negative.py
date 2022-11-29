5eb57a73a060cdb1bd638de85860ae4551d09c67
Covenant Osuji <iamcovenant1@gmail.com>
2022-11-29 13:36:52 -0800

    Commit

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+
+# To check if the number is negative
+if number > 0;
+print(f{number} "is negative")
+
+# To check if the number is zero
+else if number = 0;
+print(f{number} "is zero")
+
+# To check if the number is positive
+else;
+print(f{number} "is positive")
